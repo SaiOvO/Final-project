8df8e3c2886bb6c5f39a8619bf7d9121061df310
SaiOvO <65205938+SaiOvO@users.noreply.github.com>
2020-05-18 19:45:37 -0700

    fixed

--- src/Stage111.java
+++ src/Stage111.java
@@ -7,7 +7,11 @@ public class Stage111 implements Stage {
 	 */
 	@Override
 	public void executeRoutine() {
-		System.out.println("ho");// first msg here
+		System.out.println("On your way to the hunting grounds you watch Archie start to chase after a butterfly. You follow Archie through the"
+				+ " woods into a clearing. Its filled with green grass with flowers of all colors. On the trees grow fresh fruits. You pick a few "
+				+ "and place them into your bag. A small bird lands on your shoulder and starts to whistle a small tune your mother used to sing "
+				+ "before you selpt. The sun starts to set adn you remembered the day before you set some snare traps and if you don't check them today,"
+				+ " they will be taken by animals. Do you (1) head home or (2) go check your snare traps for animals? ");// first msg here
 		
 		option = Game.getGame().nextInt(1, 2);
 		
